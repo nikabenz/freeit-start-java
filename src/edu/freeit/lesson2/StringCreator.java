0dc3c645f4b63f4ed85cb45ccce747c59729461e
Nika B <nika.benz.21@gmail.com>
2022-06-27 20:02:41 +0300

    Homework2 add string-literal

--- src/edu/freeit/lesson2/StringCreator.java
+++ src/edu/freeit/lesson2/StringCreator.java
@@ -7,16 +7,12 @@ public class StringCreator {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter your text for string ");
         String text = scanner.nextLine();
-        String str1 = createFromLiteral(text);
-        String str2 = createFromNew(text);
+        String str1 = "Its literal here ";
+        String str2 = new String(text);
         String str3 = str1 + str2;
+        System.out.println("creating string from concatenation: " + str3);
         String str4 = args[0];
-        String str5 = str4.substring(1);
-    }
-    private static String createFromLiteral(String literal) {
-        return literal;
-    }
-    private static String createFromNew(String text) {
-        return new String(text);
+        String str5 = str1.substring(1);
+        System.out.println("creating string from substring: " + str5);
     }
 }
