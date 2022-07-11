10fb4140353ee577ab8facc2dfb87f29ad79b0cb
Nika B <nika.benz.21@gmail.com>
2022-07-11 21:41:25 +0300

    Homework6 last update

--- src/edu/freeit/lesson6/Main.java
+++ src/edu/freeit/lesson6/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Person mars = new Person(1, "mars");
         NoteBook noteBook = new NoteBook(11, " diary");
         noteBook.setOwner(mars);
-        //Note note1 = new Note(110, "read a book in English");
-        //noteBook.addNote(note1, 0);
+        Note note1 = new Note(110, "read a book in English");
+        noteBook.addNote(note1, 0);
         Note note2 = new Note(111, "read a magazine");
         noteBook.addNote(note2, 1);
         Note note3 = new Note(112, "write and read articles, analyze articles");
@@ -19,12 +19,12 @@ public class Main {
             }
             Thread.sleep(4000);
         }
-//        System.out.println("----------------------------------------------------------------------------------------output before to sort");
-//
-//        noteBook.print();
-//        System.out.println("----------------------------------------------------------------------------------------output after to sort");
-//        noteBook.sort();
-//        noteBook.print();
+        System.out.println("----------------------------------------------------------------------------------------output before to sort");
+
+        noteBook.print();
+        System.out.println("----------------------------------------------------------------------------------------output after to sort");
+        noteBook.sort();
+        noteBook.print();
         System.out.println("----------------------------------------------------------------------------------------output frequency dictionary");
 
         System.out.println(noteBook.defineFrequencyIndividualCase("read"));
